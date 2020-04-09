0cdfdbb79912a589417f50a4ed1db7781976a429
Ankush Vij <45926285+vijankush@users.noreply.github.com>
2020-04-09 13:34:46 -0400

    Create aws-cli-nodejs-wrapper.js

---
+++ Kinesis/aws-cli-nodejs-wrapper.js
@@ -0,0 +1,31 @@
+/*
+This is a snippet of a NodeJS application that I made that sends data from file line-by-line to kinesis stream through AWS CLI Command-line interface
+*/
+
+var awsCli = require('aws-cli-js');
+var Options = awsCli.Options;
+var Aws = awsCli.Aws;
+
+const aws = new Aws();
+var count = 0;
+
+var LineByLineReader = require('line-by-line'),
+  lr= new LineByLineReader('/Users/ankushvij/Desktop/AWS_Stuff/March_30/raw_log');
+
+lr.on('line', function (str) {
+  try {
+    aws.command('kinesis put-record --cli-binary-format raw-in-base64-out --stream-name kush_kinesis --partition-key 123 --data ' + "'" + str + "'");
+    count++;
+    if (count % 100 == 0) {
+        lr.pause();
+        console.log("# of Data sent: " + count);
+        lr.resume();
+      }
+    } catch (err) {
+      console.log(`err: ${err}\n`);
+    }
+});
+
+lr.on('end', function () {
+	console.log("Done Total: " + count);
+});
